5d4af3381fcba2448bea2f6e524a1ffb8f406758
Yusuf Demirci <demirciy94@gmail.com>
2020-05-25 12:16:05 +0300

    - Added new DKeychainManager, - Integrated fastlane to travis.

--- Demirciy/Classes/DKeychainManager.swift
+++ Demirciy/Classes/DKeychainManager.swift
@@ -6,35 +6,67 @@
 //  Copyright © 2019 Yusuf Demirci. All rights reserved.
 //
 
-public class DKeychainManager {}
-
-// MARK: - Public Functions
-public extension DKeychainManager {
+public class DKeychainManager {
     
-    class func save(key: String, data: Data) -> OSStatus {
-        let query = [
-            kSecClass as String: kSecClassGenericPassword as String,
-            kSecAttrAccount as String: key,
-            kSecValueData as String: data
-            ] as [String: Any]
-
-        SecItemDelete(query as CFDictionary)
-
-        return SecItemAdd(query as CFDictionary, nil)
+    // MARK: - Properties
+    public static let shared: DKeychainManager = DKeychainManager()
+    
+    open subscript(key: String) -> String? {
+        get {
+            return get(key: key)
+        } set {
+            DispatchQueue.global().sync(flags: .barrier) {
+                self.set(key: key, value: newValue)
+            }
+        }
     }
+}
 
-    class func get<T>(key: String) -> T? {
-        let query = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: key,
-            kSecReturnData as String: kCFBooleanTrue!,
-            kSecMatchLimit as String: kSecMatchLimitOne
-            ] as [String: Any]
+// MARK: - Private Functions
+private extension DKeychainManager {
+    
+    private func set(key: String, value: String?) {
+        let query = keychainQuery(key: key)
+        let objectData: Data? = value?.data(using: String.Encoding.utf8, allowLossyConversion: false)
 
-        var dataTypeRef: AnyObject? = nil
-
-        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        if SecItemCopyMatching(query, nil) == noErr {
+            if let dictData = objectData {
+                SecItemUpdate(query, NSDictionary(dictionary: [kSecValueData: dictData]))
+                return
+            }
+            
+            SecItemDelete(query)
+            
+            return
+        }
         
-        return status == noErr ? dataTypeRef as? T : nil
+        if let dictData = objectData {
+            query.setValue(dictData, forKey: kSecValueData as String)
+            SecItemAdd(query, nil)
+        }
+    }
+    
+    private func get(key: String) -> String? {
+        let query = keychainQuery(key: key)
+        query.setValue(kCFBooleanTrue, forKey: kSecReturnData as String)
+        query.setValue(kCFBooleanTrue, forKey: kSecReturnAttributes as String)
+        
+        var result: CFTypeRef?
+        let status = SecItemCopyMatching(query, &result)
+        
+        guard
+            let resultsDict = result as? NSDictionary,
+            let resultsData = resultsDict.value(forKey: kSecValueData as String) as? Data,
+            status == noErr
+            else { return nil }
+        return String(data: resultsData, encoding: String.Encoding.utf8)
+    }
+    
+    private func keychainQuery(key: String) -> NSMutableDictionary {
+        let result = NSMutableDictionary()
+        result.setValue(kSecClassGenericPassword, forKey: kSecClass as String)
+        result.setValue(key, forKey: kSecAttrService as String)
+        result.setValue(kSecAttrAccessibleAfterFirstUnlock, forKey: kSecAttrAccessible as String)
+        return result
     }
 }
